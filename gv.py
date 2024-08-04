47a3429fe3c1ac287b69ba4f4884ab0ed3fdb562
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-04 23:59:03 +0530

    GV page fixed

--- gv.py
+++ gv.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 def gv_page():
 
-    col1, col2, col3, col4, col5 = st.columns([1, 2.2, 2, 1])
+    col1, col2, col3, col4, col5 = st.columns([1, 2, 2, 2, 1])
 
     with col2:
         st.image(value.GV_1)
