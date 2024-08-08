e0061a4f625e2f47ae5c8a31b77a4528d540ddc6
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-08 23:41:33 +0530

    GV Content added

--- gv.py
+++ gv.py
@@ -21,3 +21,52 @@ def gv_page():
 
     with col4:
         st.image(value.GV_3)
+
+    col6, col7, col8, col9, col10 = st.columns([1, 2, 2, 2, 1])
+
+    with col7:
+        st.image(value.GV_4)
+
+    with col8:
+        st.image(value.GV_5)
+
+    with col9:
+        st.image(value.GV_6)
+
+    col11, col12, col13, col14, col15 = st.columns([1, 2, 2, 2, 1])
+
+    with col12:
+        st.image(value.GV_7)
+
+    with col13:
+        st.image(value.GV_8)
+
+    with col14:
+        st.image(value.GV_9)
+
+    col15, col16, col17, col18, col9 = st.columns([1, 2, 2, 2, 1])
+
+    with col6:
+        st.image(value.GV_10)
+
+    with col7:
+        st.image(value.GV_11)
+
+    with col8:
+        st.image(value.GV_12)
+
+    col20, col21, col22, col23, col24 = st.columns([1, 2, 2, 2, 1])
+
+    with col21:
+        st.image(value.GV_13)
+
+    with col22:
+        st.image(value.GV_14)
+
+    with col23:
+        st.image(value.GV_15)
+
+    col25, col26, col27, col28, col29 = st.columns([1, 2, 2, 2, 1])
+
+    with col26:
+        st.image(value.GV_16)
