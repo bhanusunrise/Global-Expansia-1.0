2dd72ef6aa892bb64ccaa09f65d5d6aa26574760
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-17 02:13:14 +0530

    Global links added

--- web.py
+++ web.py
@@ -12,9 +12,9 @@ col1, col2, col3 = st.columns([1, 9, 1])
 
 with col2:
 
-    st.image("images/10.png", caption="", width=20, use_column_width="always",
+    st.image(value.EVENT_LOGO, caption="", width=20, use_column_width="always",
              clamp="", channels="RGB", output_format="auto")
 
     comp.infomation_package(" ", [
-        "Global Expansia organized by AIESEC in University of Kelaniya, is an event focused on personal branding through global volunteering and internships. Our program includes online sessions designed to educate delegates about international volunteering and internship opportunities while helping them build a strong personal brand. The series culminates in a final physical event offering a unique chance to secure life-changing opportunities and enhance their personal brand on a global scale."
+        value.EVENT_DESCRIPTION_HOME
     ])
