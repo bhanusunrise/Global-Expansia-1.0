cd17b7b5bec60d336da851ef64ebdbfcc3509175
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-20 01:33:18 +0530

    Multipage deploy tested 5

--- components.py
+++ components.py
@@ -245,8 +245,8 @@ def register_now_button():
 def see_more_button(page_link):
     st.markdown(f"""
                 <center>
-                    <a href="{page_link}">
-                        <button target="_self" style="background-color:#003577; border-color: white; border-radius: 20px; padding-top: 10px; padding-left: 30px; padding-right: 30px; padding-bottom: 10px;">See More</button>
+                    <a href="{page_link}" target="_self">
+                        <button style="background-color:#003577; border-color: white; border-radius: 20px; padding-top: 10px; padding-left: 30px; padding-right: 30px; padding-bottom: 10px;">See More</button>
                     </a>
                 </center>
                 """,
