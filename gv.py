51667c032131c82cc4c832758bdc8036c6ca6c77
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-21 00:19:12 +0530

    Gv updated 2

--- gv.py
+++ gv.py
@@ -11,7 +11,7 @@ def gv_page():
 
     comp.create_gap(1)
 
-    col1, col2, col3, col4, col5 = st.columns([1, 3, 3, 3, 1])
+    col1, col2, col3, col4, col5 = st.columns([1, 10, 10, 10, 1])
 
     with col2:
         st.image(value.GV_1)
@@ -22,7 +22,7 @@ def gv_page():
     with col4:
         st.image(value.GV_3)
 
-    col6, col7, col8, col9, col10 = st.columns([1, 3, 3, 3, 1])
+    col6, col7, col8, col9, col10 = st.columns([1, 10, 10, 10, 1])
 
     with col7:
         st.image(value.GV_4)
@@ -33,7 +33,7 @@ def gv_page():
     with col9:
         st.image(value.GV_6)
 
-    col11, col12, col13, col14, col15 = st.columns([1, 3, 3, 3, 1])
+    col11, col12, col13, col14, col15 = st.columns([1, 10, 10, 10, 1])
 
     with col12:
         st.image(value.GV_7)
@@ -44,7 +44,7 @@ def gv_page():
     with col14:
         st.image(value.GV_9)
 
-    col15, col16, col17, col18, col9 = st.columns([1, 3, 3, 3, 1])
+    col15, col16, col17, col18, col19 = st.columns([1, 10, 10, 10, 1])
 
     with col16:
         st.image(value.GV_10)
@@ -55,7 +55,7 @@ def gv_page():
     with col18:
         st.image(value.GV_12)
 
-    col20, col21, col22, col23, col24 = st.columns([1, 3, 3, 3, 1])
+    col20, col21, col22, col23, col24 = st.columns([1, 10, 10, 10, 1])
 
     with col21:
         st.image(value.GV_13)
@@ -66,7 +66,7 @@ def gv_page():
     with col23:
         st.image(value.GV_15)
 
-    col25, col26, col27, col28, col29 = st.columns([1, 3, 3, 3, 1])
+    col25, col26, col27, col28, col29 = st.columns([1, 10, 10, 10, 1])
 
     with col27:
         st.image(value.GV_5)
