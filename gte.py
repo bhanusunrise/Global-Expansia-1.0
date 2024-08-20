262233119e51f497e8df11439974d565f37f545a
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-21 00:22:36 +0530

    Gte updated

--- gte.py
+++ gte.py
@@ -11,7 +11,7 @@ def gte_page():
 
     comp.create_gap(1)
 
-    col1, col2, col3, col4, col5 = st.columns([1, 2, 2, 2, 1])
+    col1, col2, col3, col4, col5 = st.columns([1, 10, 10, 10, 1])
 
     with col2:
         st.image(value.GTE_1)
@@ -22,19 +22,21 @@ def gte_page():
     with col4:
         st.image(value.GTE_6)
 
-    col6, col7, col8, col9, col10 = st.columns([1, 2, 2, 2, 1])
+    col6, col7, col8, col9, col10 = st.columns([1, 10, 10, 10, 1])
 
     with col7:
         st.image(value.GTE_8)
+
     with col8:
         st.image(value.GTE_5)
 
     with col9:
         st.image(value.GTE_8)
 
-    col11, col12, col13, col14, col15 = st.columns([1, 2, 2, 2, 1])
+    col11, col12, col13, col14, col15 = st.columns([1, 10, 10, 10, 1])
 
     with col12:
         st.image(value.GTE_7)
+
     with col14:
         st.image(value.GTE_2)
