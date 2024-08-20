f76c0e3236774b0493a2bf5d5c25a01596b0da12
bhanusunrise <pasindubhanukagood@gmail.com>
2024-08-21 00:20:43 +0530

    Gta updated

--- gta.py
+++ gta.py
@@ -11,7 +11,7 @@ def gta_page():
 
     comp.create_gap(1)
 
-    col1, col2, col3, col4, col5 = st.columns([1, 2, 2, 2, 1])
+    col1, col2, col3, col4, col5 = st.columns([1, 10, 10, 10, 1])
 
     with col2:
         st.image(value.GTA_1)
@@ -22,7 +22,7 @@ def gta_page():
     with col4:
         st.image(value.GTA_4)
 
-    col6, col7, col8, col9, col10 = st.columns([1, 2, 2, 2, 1])
+    col6, col7, col8, col9, col10 = st.columns([1, 10, 10, 10, 1])
 
     with col7:
         st.image(value.GTA_5)
